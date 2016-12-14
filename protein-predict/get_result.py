1ab3fc5a676ce050079e8a1af3bfbcc30e804e99
vrElessar <N16054572@mail.ncku.edu.tw>
2016-12-15 02:06:03 +0800

    clean result file

---
+++ protein-predict/get_result.py
@@ -0,0 +1,7 @@
+resultFile = open("./predict-result","r")
+cleanResult = open("./clean-result", "w")
+next(resultFile)
+for line in resultFile:
+    cleanResult.write(line.split()[0] + " ")
+resultFile.close()
+cleanResult.close()
