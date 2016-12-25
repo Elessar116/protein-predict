a9ebcc4075148bc17e10e712cd333e2dc0dbb714
vrElessar <N16054572@mail.ncku.edu.tw>
2016-12-25 19:48:33 +0800

    feature 5

--- protein-predict/feature-evaluate.py
+++ protein-predict/feature-evaluate.py
@@ -1,20 +1,27 @@
-testData = open("test-data","r")
+﻿testData = open("test-data","r")
 
 posCount = 0
 negCount = 0
-for line in testData:
-    if float(line.split()[1][2:])==8 and int(line.split()[0])==1:
-        posCount += 1
-    elif float(line.split()[1][2:])==8 and int(line.split()[0])==-1:
-        negCount += 1
-print(posCount,negCount)
+for feature in range(1,21):
+    print(str(feature))
+    for line in testData:
+        #print(line.split()[5][2:])
+        if int(line.split()[5][2:])==feature and int(line.split()[0])==1:
+            posCount += 1
+        elif int(line.split()[5][2:])==feature and int(line.split()[0])==-1:
+            negCount += 1
+    print(posCount,negCount)
+    
 
-trainData = open("train-data","r")
-posCount2 = 0
-negCount2 = 0
-for line in trainData:
-    if float(line.split()[1][2:])==8 and int(line.split()[0])==1:
-        posCount2 += 1
-    elif float(line.split()[1][2:])==8 and int(line.split()[0])==-1:
-        negCount2 += 1
-print(posCount2/5,negCount2)
+    trainData = open("train-data","r")
+    posCount2 = 0
+    negCount2 = 0
+    for line in trainData:
+        if int(line.split()[5][2:])==feature and int(line.split()[0])==1:
+            posCount2 += 1
+        elif int(line.split()[5][2:])==feature and int(line.split()[0])==-1:
+            negCount2 += 1
+    print(int(posCount2/5),negCount2)
+    print("\n")
+testData.close()
+trainData.close()
