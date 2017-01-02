c3078c389ac4d274785518a5928e962f9951b780
Elessar <unilord9@gmail.com>
2017-01-03 02:18:41 +0800

    evaluate

--- protein-predict/feature-evaluate.py
+++ protein-predict/feature-evaluate.py
@@ -1,11 +1,11 @@
-testData = open("test-data","r")
+﻿testData = open("test-data","r")
 
 posCount = 0
 negCount = 0
 for line in testData:
-    if float(line.split()[3][2:])>300 and int(line.split()[0])==1:
+    if float(line.split()[3][2:])<0.5 and int(line.split()[0])==1:
         posCount += 1
-    elif float(line.split()[3][2:])>300 and int(line.split()[0])==-1:
+    elif float(line.split()[3][2:])<0.5 and int(line.split()[0])==-1:
         negCount += 1
 print(posCount,negCount)
 
@@ -13,8 +13,8 @@ trainData = open("train-data","r")
 posCount2 = 0
 negCount2 = 0
 for line in trainData:
-    if float(line.split()[3][2:])>300 and int(line.split()[0])==1:
+    if float(line.split()[3][2:])<0.5 and int(line.split()[0])==1:
         posCount2 += 1
-    elif float(line.split()[3][2:])>300 and int(line.split()[0])==-1:
+    elif float(line.split()[3][2:])<0.5 and int(line.split()[0])==-1:
         negCount2 += 1
 print(int(posCount2/5),negCount2)
